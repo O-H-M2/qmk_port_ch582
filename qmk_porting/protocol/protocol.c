42e02e890067aa170ee9b57a6a327d9c8aff5f43
Wyatt_Zhang <46043032+Huckies@users.noreply.github.com>
2022-07-22 10:25:18 +0800

    working on usb descriptor

--- qmk_porting/protocol/protocol.c
+++ qmk_porting/protocol/protocol.c
@@ -3,12 +3,12 @@
 #include "host.h"
 
 uint8_t keyboard_leds(void);
-void    send_keyboard(report_keyboard_t *report);
-void    send_mouse(report_mouse_t *report);
-void    send_system(uint16_t data);
-void    send_consumer(uint16_t data);
-void    send_programmable_button(uint32_t data);
-void    send_digitizer(report_digitizer_t *report);
+void send_keyboard(report_keyboard_t *report);
+void send_mouse(report_mouse_t *report);
+void send_system(uint16_t data);
+void send_consumer(uint16_t data);
+void send_programmable_button(uint32_t data);
+void send_digitizer(report_digitizer_t *report);
 
 host_driver_t ch582_driver = { keyboard_leds, send_keyboard, send_mouse, send_system, send_consumer, send_programmable_button };
 
@@ -20,6 +20,9 @@ void protocol_setup()
 void protocol_pre_init()
 {
     usb_dc_low_level_init();
+    // TODO: finish this
+    // usb_event_queue_init();
+    init_usb_driver();
 }
 
 void protocol_post_init()
