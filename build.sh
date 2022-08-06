79ce5917682e25c02455c7bd17dee314af26765f
张承儒 <companion244@gmail.com>
2022-08-06 13:32:35 +0800

    switched to ninja

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh zsh
 
-cmake -G"Eclipse CDT4 - Unix Makefiles"  -DCMAKE_BUILD_TYPE=Release && \
-make -j8
+cmake --build /Users/huckies/Downloads/qmk_port_ch582/build --config Release --target all --
