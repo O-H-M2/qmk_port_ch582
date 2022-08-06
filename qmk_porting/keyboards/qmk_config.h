39f96acabd2dce761f4752b4a6f149f1c4e8a165
张承儒 <companion244@gmail.com>
2022-08-06 11:48:03 +0800

    minor improvement on plf timer; make CMakeLists more user friendly

--- qmk_porting/keyboards/qmk_config.h
+++ qmk_porting/keyboards/qmk_config.h
@@ -17,6 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+// platform settings
+#define DEBUG       1
+#define DCDC_ENABLE 1
+
 #define EARLY_INIT_PERFORM_BOOTLOADER_JUMP FALSE
 
 /* USB Device descriptor parameter */
@@ -26,6 +30,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MANUFACTURER Xiaomi
 #define PRODUCT      MK02
 
+#define QMK_VERSION    "0.0.1"
+#define QMK_BUILDDATE  "Today"
+#define QMK_KEYBOARD_H "mk02.h"
+
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
 #define MATRIX_ROW_PINS      \
     {                        \
         B9, A7, B8, B16, B18 \
@@ -34,12 +44,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     {                                                            \
         A4, A5, A6, A0, A1, A3, B17, B5, B4, B3, B2, B1, B0, B21 \
     }
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION       COL2ROW
+#define BOOTMAGIC_LITE_ROW    1
+#define BOOTMAGIC_LITE_COLUMN 1
+
+#define MOUSE_ENABLE    1
+#define RAW_ENABLE      1
+#define EEPROM_ENABLE   1
+#define RGBLIGHT_ENABLE 1
+#define COMMAND_ENABLE  1
+
+#define RGBLED_NUM 61
 
 #ifdef RGBLIGHT_ENABLE
 // #define RGBLIGHT_LIMIT_VAL 100
-#define RGB_DI_PIN         A14
-#define WS2812_BYTE_ORDER  WS2812_BYTE_ORDER_RGB
+#define RGB_DI_PIN        A14
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
+#define RGBLIGHT_LIMIT_VAL 100
 #endif
 
 /* define if matrix has ghost */
--- qmk_porting/platforms/ch58x/timer.c
+++ qmk_porting/platforms/ch58x/timer.c
@@ -5,6 +5,10 @@ void timer_init(void)
     R32_TMR0_CNT_END = FREQ_SYS / 1000;
     R8_TMR0_CTRL_MOD = RB_TMR_ALL_CLEAR;
     R8_TMR0_CTRL_MOD = RB_TMR_COUNT_EN;
+    // clear interrupt bits
+    R8_TMR0_INTER_EN = 0;
+    // and disable interrupt
+    PFIC_EnableIRQ(TMR0_IRQn);
 }
 
 uint16_t timer_read(void)
