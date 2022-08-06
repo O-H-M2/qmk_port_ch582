fc54b16ce1c6d82e6e6d2d973dfac3b2c9bc84ed
张承儒 <companion244@gmail.com>
2022-08-06 17:34:09 +0800

    rgb in progress

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh zsh
 
-cmake --build /Users/huckies/Downloads/qmk_port_ch582/build --config Release --target all --
+cmake --build /Users/huckies/Downloads/qmk_port_ch582/build --config Release --target all -- && \
+cp ./build/qmk_port_ch582.hex ./qmk_port_ch582.hex
