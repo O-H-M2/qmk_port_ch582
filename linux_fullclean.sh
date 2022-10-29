c8c65c0846ab00e05bcf7ad77ec47f6ebec603b0
fafa <1254333071@qq.com>
2022-10-29 22:27:33 +0800

    添加Linux平台下的编译和清除脚本

---
+++ linux_fullclean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf ./build
+rm -rf ./CMakeFiles
+rm .cproject
+rm .project
+rm .ninja_deps
+rm .ninja_log
+rm build.ninja
+rm cmake_install.cmake
+rm CMakeCache.txt
+rm Makefile
+rm *.bin
+rm *.hex
+rm *.uf2
