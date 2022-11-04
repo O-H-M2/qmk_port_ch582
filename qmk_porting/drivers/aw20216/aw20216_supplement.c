34caa453bf72871342c5e56850123ce7353ba1ed
Wyatt_Zhang <46043032+Huckies@users.noreply.github.com>
2022-11-04 10:59:36 +0800

    minor improvement

--- qmk_porting/drivers/aw20216/aw20216_supplement.c
+++ qmk_porting/drivers/aw20216/aw20216_supplement.c
@@ -1,3 +1,20 @@
+/*
+Copyright 2022 Huckies <https://github.com/Huckies>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "aw20216_supplement.h"
 
 static volatile bool aw20216_powered_on = true, aw20216_need_power_off = false;
