1e4396b8a7d3da2b878affefa26aa46f4bd3dacf
Wyatt_Zhang <46043032+Huckies@users.noreply.github.com>
2022-11-26 10:08:16 +0800

    took ble descriptor out and removed wireless submodule; added support for using libs; minor syntax improvement on bootloader

--- qmk_porting/platforms/ch58x/bootloader.h
+++ qmk_porting/platforms/ch58x/bootloader.h
@@ -28,68 +28,7 @@ extern volatile uint8_t kbd_protocol_type;
 
 void bootloader_boot_mode_set(uint8_t mode);
 uint8_t bootloader_boot_mode_get();
+void bootloader_select_boot_mode();
+uint8_t bootloader_set_to_default_mode(const char *reason);
 void bootloader_jump();
 void mcu_reset();
-
-__attribute__((always_inline)) inline uint8_t bootloader_set_to_default_mode(const char *reason)
-{
-    PRINT("%s, ", reason);
-#ifdef USB_ENABLE
-    PRINT("default to USB.\n");
-    bootloader_boot_mode_set(BOOTLOADER_BOOT_MODE_USB);
-    return BOOTLOADER_BOOT_MODE_USB;
-#elif defined BLE_ENABLE
-    PRINT("default to BLE slot 0.\n");
-    bootloader_boot_mode_set(BOOTLOADER_BOOT_MODE_BLE);
-    return BOOTLOADER_BOOT_MODE_BLE;
-#else
-    PRINT("default to ESB.\n");
-    bootloader_boot_mode_set(BOOTLOADER_BOOT_MODE_ESB);
-    return BOOTLOADER_BOOT_MODE_ESB;
-#endif
-}
-
-__attribute__((always_inline)) inline void bootloader_select_boot_mode()
-{
-    uint8_t mode = bootloader_boot_mode_get();
-
-    if (mode == BOOTLOADER_BOOT_MODE_IAP) {
-        mode = bootloader_set_to_default_mode("Successfully booted from IAP");
-    }
-
-    //! TODO: for test only!
-    // mode = BOOTLOADER_BOOT_MODE_BLE;
-
-    switch (mode) {
-#ifdef USB_ENABLE
-        case BOOTLOADER_BOOT_MODE_USB:
-            // cable mode
-            kbd_protocol_type = kbd_protocol_usb;
-            break;
-#endif
-#ifdef BLE_ENABLE
-        case BOOTLOADER_BOOT_MODE_BLE:
-            // bluetooth mode
-            kbd_protocol_type = kbd_protocol_ble;
-            break;
-#endif
-#ifdef ESB_ENABLE
-        case BOOTLOADER_BOOT_MODE_ESB:
-            // 2.4g mode
-            kbd_protocol_type = kbd_protocol_esb;
-            break;
-#endif
-        default:
-            PRINT("Invalid mode record detected, ");
-            if (mode == BOOTLOADER_BOOT_MODE_IAP) {
-                PRINT("will reside in IAP.\n");
-            } else {
-                bootloader_boot_mode_set(BOOTLOADER_BOOT_MODE_IAP);
-                mode = bootloader_boot_mode_get();
-                PRINT("set to IAP... %s\n", mode == BOOTLOADER_BOOT_MODE_IAP ? "done" : "fail");
-            }
-            PRINT("Reboot execute.\n");
-            WAIT_FOR_DBG;
-            SYS_ResetExecute();
-    }
-}
