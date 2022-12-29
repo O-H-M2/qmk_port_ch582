f962283c7b259bf7ad5f8760c590ea342f417c34
OctopusZ <928281509@qq.com>
2022-12-29 16:33:59 +0800

    add Ezy64 (#23)
    add ezy64
    
    Co-authored-by: Wyatt_Zhang <46043032+Huckies@users.noreply.github.com>

---
+++ qmk_porting/keyboards/ezy64/ezy64.c
@@ -0,0 +1,113 @@
+/*
+Copyright 2022 OctopusZ <https://github.com/OctopusZ>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "battery_measure.h"
+
+#ifdef RGB_MATRIX_ENABLE
+
+/* clang-format off */
+led_config_t g_led_config = {
+	{
+        {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,  11,  12,  13}, 
+        {27,  26,  25,  24,  23,  22,  21,  20,  19,  18,  17,  16,  15,  14}, 
+        {28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,   NO_LED}, 
+        {54,  53,  52,  51,  50,  49,  48,  47,  46,  45,  44,  43,  42,  41}, 
+        {55,  56,  57,  58,  59,   NO_LED,   NO_LED,   NO_LED,  60,   NO_LED,   NO_LED,  61,  62,  63}, 
+
+
+	}, {
+        {0,0},{17,0},{34,0},{52,0},{69,0},{86,0},{103,0},{121,0},{138,0},{155,0},{172,0},{190,0},{207,0},{224,0},
+        {224,16},{207,16},{190,16},{172,16},{155,16},{138,16},{121,16},{103,16},{86,16},{69,16},{52,16},{34,16},{17,16},{0,16},
+        {0,32},{17,32},{34,32},{52,32},{69,32},{86,32},{103,32},{121,32},{138,32},{155,32},{172,32},{190,32},{207,32},       
+        {224,48},{207,48},{190,48},{172,48},{155,48},{138,48},{121,48},{103,48},{86,48},{69,48},{52,48},{34,48},{17,48},{0,48},
+        {0,64},{17,64},{34,64},{52,64},{69,64},                     {138,64},              {190,64},{207,64},{224,64},
+
+	}, {
+		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
+		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,  
+		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
+		4, 4, 4, 4, 4, 4, 4, 4, 4,
+	}
+};
+
+/* clang-format on */
+
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max)
+{
+    if (host_keyboard_led_state().caps_lock) {
+        if (g_led_config.flags[0] & LED_FLAG_KEYLIGHT) {
+            rgb_matrix_set_color(28, RGB_RED);
+        }
+    }
+    // battery indicator
+    if (battery_indicator_state()) {
+        if (!battery_indicator_timeout()) {
+            rgb_matrix_set_color_all(0, 0, 0); //turn off all leds
+            for (uint8_t i = 0; i < 5; i++) {
+                rgb_matrix_set_color(i, RGB_RED);
+            }
+
+            uint8_t battery_indicator_num = 5;
+
+            battery_indicator_num = 30 / 20;
+            for (uint8_t i = 0; i < battery_indicator_num; i++) {
+                rgb_matrix_set_color(i, RGB_GREEN);
+            }
+        } else {
+            battery_indicator_toggle(false);
+        }
+    }
+}
+
+#endif
+
+#if !defined ESB_ENABLE || ESB_ENABLE != 2
+
+int main()
+{
+    extern void protocol_setup();
+    extern void protocol_init();
+    extern void protocol_task();
+
+    platform_setup();
+
+    protocol_setup();
+    keyboard_setup();
+
+    protocol_init();
+
+    /* Main loop */
+    for (;;) {
+        protocol_task();
+        //! housekeeping_task() is handled by platform
+    }
+}
+
+#else
+
+__HIGH_CODE int main()
+{
+    platform_setup();
+
+    for (;;) {
+        TMOS_SystemProcess();
+    }
+}
+
+#endif
---
+++ qmk_porting/keyboards/ezy64/qmk_config.h
@@ -0,0 +1,130 @@
+/*
+Copyright 2022 OctopusZ <https://github.com/OctopusZ>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//* platform settings
+// #define DEBUG        Debug_UART1
+#define DCDC_ENABLE  1
+#define FREQ_SYS     40000000
+#define LSE_ENABLE   0
+#define BLE_SLOT_NUM 4
+// #define HSE_LOAD_CAPACITANCE 7.5 // in pF unit
+// #define LSE_LOAD_CAPACITANCE 7   // in pF unit
+// #define I2C_IO_REMAPPING
+// #define SPI_IO_REMAPPING
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0xCAFE
+#define PRODUCT_ID   0x0B91
+#define DEVICE_VER   0x0001
+#define MANUFACTURER ezy64
+#define PRODUCT      ezy64
+
+#define QMK_VERSION    "0.0.1"
+#define QMK_BUILDDATE  __DATE__
+#define QMK_KEYBOARD_H "ezy64.h"
+
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
+#define MATRIX_ROW_PINS       \
+    {                         \
+        B9, B8, B17, B16, B18 \
+    }
+#define MATRIX_COL_PINS                                            \
+    {                                                              \
+        A6, A0, A1, A2, A3, A15, A14, A13, A12, A7, B7, B6, B5, B4 \
+    }
+#define DIODE_DIRECTION       COL2ROW
+#define BOOTMAGIC_LITE_ROW    0
+#define BOOTMAGIC_LITE_COLUMN 0
+// #define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+
+#define EARLY_INIT_PERFORM_BOOTLOADER_JUMP FALSE
+
+#define WS2812_EN_PIN   A5
+#define WS2812_EN_LEVEL 1
+
+#define BATTERY_MEASURE_PIN A4
+
+#ifdef RGB_MATRIX_ENABLE
+#define WS2812_SPI_DRIVER             2
+#define RGBLED_NUM                    64
+#define DRIVER_LED_TOTAL              RGBLED_NUM
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 32
+#define RGB_MATRIX_STARTUP_VAL        RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#define RGB_MATRIX_HUE_STEP           10
+#define RGB_MATRIX_SAT_STEP           8
+#define RGB_MATRIX_VAL_STEP           4
+#define RGB_MATRIX_SPD_STEP           10
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_BAND_SAT
+#define ENABLE_RGB_MATRIX_BAND_VAL
+#define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#define ENABLE_RGB_MATRIX_CYCLE_ALL
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#define ENABLE_RGB_MATRIX_DUAL_BEACON
+#define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#define ENABLE_RGB_MATRIX_RAINDROPS
+#define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#define ENABLE_RGB_MATRIX_HUE_BREATHING
+#define ENABLE_RGB_MATRIX_HUE_PENDULUM
+#define ENABLE_RGB_MATRIX_HUE_WAVE
+#define ENABLE_RGB_MATRIX_PIXEL_RAIN
+#define ENABLE_RGB_MATRIX_PIXEL_FLOW
+#define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+
+#endif
+
+/* define if matrix has ghost */
+//#define MATRIX_HAS_GHOST
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 10
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#include "pre_handler.h"
