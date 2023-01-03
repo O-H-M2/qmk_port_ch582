b78d8ae2db6c48c7cdb470834fadf8efd5f7487f
Wyatt_Zhang <46043032+Huckies@users.noreply.github.com>
2023-01-03 19:16:10 +0800

    abandoned seperate descriptor for ble interface

--- qmk_porting/common_features.cmake
+++ qmk_porting/common_features.cmake
@@ -261,9 +261,6 @@ if(BLE_ENABLE)
         add_definitions(-DBLE_ENABLE)
         message(STATUS "BLE_ENABLE: Nkro is forced enable")
         add_definitions(-DNKRO_ENABLE -DFORCE_NKRO)
-        list(APPEND QMK_PORTING_SOURCES
-            "${CMAKE_CURRENT_LIST_DIR}/protocol/ble_descriptor.c"
-        )
     endif()
 endif()
 
