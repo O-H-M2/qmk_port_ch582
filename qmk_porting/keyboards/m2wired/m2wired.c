94f938b1c56947da5f8c99a222c26e3c352aaa86
OctopusZ <928281509@qq.com>
2023-01-06 01:50:29 +0800

    fix something about m2wired

--- qmk_porting/keyboards/m2wired/m2wired.c
+++ qmk_porting/keyboards/m2wired/m2wired.c
@@ -45,13 +45,14 @@ led_config_t g_led_config = {
 };
 /* clang-format on */
 
-void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max)
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max)
 {
     if (host_keyboard_led_state().num_lock) {
         if (g_led_config.flags[0] & LED_FLAG_KEYLIGHT) {
             rgb_matrix_set_color(0, RGB_RED);
         }
     }
+    return 1;
 }
 
 #endif
