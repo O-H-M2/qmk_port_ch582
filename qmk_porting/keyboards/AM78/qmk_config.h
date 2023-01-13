8717bd8be46701529b8f68917c65d692ca33cd35
Wyatt_Zhang <46043032+Huckies@users.noreply.github.com>
2023-01-13 12:34:09 +0800

    enabled LSE for ezy64 and AM78

--- qmk_porting/keyboards/AM78/qmk_config.h
+++ qmk_porting/keyboards/AM78/qmk_config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define DEBUG        Debug_UART1
 #define DCDC_ENABLE  1
 #define FREQ_SYS     40000000
-#define LSE_ENABLE   0
+#define LSE_ENABLE   1
 #define BLE_SLOT_NUM 4
 // #define HSE_LOAD_CAPACITANCE 7.5 // in pF unit
 // #define LSE_LOAD_CAPACITANCE 7   // in pF unit
