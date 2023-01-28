3f6900ebbe17ecdafcc1b4646a52641d68075f04
Wyatt_Zhang <46043032+Huckies@users.noreply.github.com>
2023-01-28 13:14:50 +0800

    minor syntax improvement on ezy64.c and fix on mk67lite.c

--- qmk_porting/keyboards/ezy64/ezy64.c
+++ qmk_porting/keyboards/ezy64/ezy64.c
@@ -27,15 +27,12 @@ led_config_t g_led_config = {
         {28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,   NO_LED}, 
         {54,  53,  52,  51,  50,  49,  48,  47,  46,  45,  44,  43,  42,  41}, 
         {55,  56,  57,  58,  59,   NO_LED,   NO_LED,   NO_LED,  60,   NO_LED,   NO_LED,  61,  62,  63}, 
-
-
 	}, {
         {0,0},{17,0},{34,0},{52,0},{69,0},{86,0},{103,0},{121,0},{138,0},{155,0},{172,0},{190,0},{207,0},{224,0},
         {224,16},{207,16},{190,16},{172,16},{155,16},{138,16},{121,16},{103,16},{86,16},{69,16},{52,16},{34,16},{17,16},{0,16},
         {0,32},{17,32},{34,32},{52,32},{69,32},{86,32},{103,32},{121,32},{138,32},{155,32},{172,32},{190,32},{207,32},       
         {224,48},{207,48},{190,48},{172,48},{155,48},{138,48},{121,48},{103,48},{86,48},{69,48},{52,48},{34,48},{17,48},{0,48},
         {0,64},{17,64},{34,64},{52,64},{69,64},                     {138,64},              {190,64},{207,64},{224,64},
-
 	}, {
 		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
 		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
@@ -44,7 +41,6 @@ led_config_t g_led_config = {
 		4, 4, 4, 4, 4, 4, 4, 4, 4,
 	}
 };
-
 /* clang-format on */
 
 bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max)
@@ -55,7 +51,7 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max)
     if (host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(28, 0xFF, 0x00, 0x00);
     }
-#ifdef BATTERY_MEASURE_PIN
+#if defined BATTERY_MEASURE_PIN || defined BLE_ENABLE
     extern void wireless_rgb_indicator_task(uint8_t led_min, uint8_t led_max);
 
     wireless_rgb_indicator_task(led_min, led_max);
