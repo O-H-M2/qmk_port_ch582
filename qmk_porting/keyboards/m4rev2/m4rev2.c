152a020b2e1e701e6adb892bbce97b34aded971b
Alin Marin Elena <alin@elena.space>
2023-02-14 08:24:07 +0000

    add simple example, cols are scanned via hc595 on spi, rows are read on pins

---
+++ qmk_porting/keyboards/m4rev2/m4rev2.c
@@ -0,0 +1,118 @@
+/*
+Copyright 2023 alin elena <https://github.com/alinelena> <https://gitlab.com/drFaustroll>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "m4rev2.h"
+
+// let us assume we start with both layers off
+static bool toggle_lwr = false;
+static bool toggle_rse = false;
+
+#ifdef RGBLIGHT_ENABLE
+
+const rgblight_segment_t PROGMEM my_qwerty_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_PURPLE});
+const rgblight_segment_t PROGMEM my_lwr_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_CYAN});
+const rgblight_segment_t PROGMEM my_rse_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_RED});
+const rgblight_segment_t PROGMEM my_adj_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_GREEN});
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(my_qwerty_layer, my_lwr_layer, my_rse_layer, my_adj_layer);
+
+#endif
+
+#if !defined ESB_ENABLE || ESB_ENABLE != 2
+
+int main()
+{
+    extern void protocol_setup();
+    extern void protocol_init();
+    extern void protocol_task();
+
+    platform_setup();
+
+    protocol_setup();
+    keyboard_setup();
+
+    protocol_init();
+
+    /* Main loop */
+    for (;;) {
+        protocol_task();
+        //! housekeeping_task() is handled by platform
+    }
+}
+
+#else
+
+__HIGH_CODE int main()
+{
+    platform_setup();
+
+    for (;;) {
+        TMOS_SystemProcess();
+    }
+}
+
+#endif
+
+
+#ifdef RGBLIGHT_ENABLE
+
+void set_rgb_layers(layer_state_t state){
+
+    rgblight_set_layer_state(0, layer_state_cmp(state, _QW));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _LWR));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _RSE));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _ADJ));
+
+}
+
+void set_default_rgb_layers(layer_state_t state){
+    rgblight_set_layer_state(0, layer_state_cmp(state, _QW));
+}
+
+const rgblight_segment_t * const* my_rgb(void){
+    return my_rgb_layers;
+}
+
+#endif
+
+void set_led_toggle(const uint8_t layer, const bool state){
+
+    switch (layer) {
+        case _LWR:
+          toggle_lwr = state;
+          break;
+        case _RSE:
+          toggle_rse = state;
+          break;
+        default:
+          break;
+    }
+}
+
+void toggle_leds(void){
+
+    led_lwr(toggle_lwr);
+    led_rse(toggle_rse);
+    led_t led_state = host_keyboard_led_state();
+    led_caps(led_state.caps_lock);
+    if (layer_state_is(_ADJ)) {
+        led_lwr(true);
+        led_rse(true);
+    }
+
+}
