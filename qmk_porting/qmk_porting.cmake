bd0f5a0af3afb785bea4f3c70d9ad4fd4a01dd7c
Wyatt_Zhang <46043032+Huckies@users.noreply.github.com>
2023-02-15 18:07:42 +0800

    minor fix on building system

--- qmk_porting/qmk_porting.cmake
+++ qmk_porting/qmk_porting.cmake
@@ -27,6 +27,7 @@ include_sub_directories_recursively(${CMAKE_CURRENT_LIST_DIR}/protocol)
 
 file(GLOB QMK_PORTING_SOURCES
     "${CMAKE_CURRENT_LIST_DIR}/platforms/ch58x/bootloader.c"
+    "${CMAKE_CURRENT_LIST_DIR}/platforms/ch58x/gpio.c"
     "${CMAKE_CURRENT_LIST_DIR}/platforms/ch58x/platform.c"
     "${CMAKE_CURRENT_LIST_DIR}/platforms/ch58x/suspend.c"
     "${CMAKE_CURRENT_LIST_DIR}/platforms/ch58x/timer.c"
