9d5c0a8f6eabb3081d947329fe74f7835863e9d8
OctopusZ <928281509@qq.com>
2023-03-03 22:13:20 +0800

    format

--- qmk_porting/keyboards/ezy64/ezy64.h
+++ qmk_porting/keyboards/ezy64/ezy64.h
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 #include "extra_keycode.h"
+
 // clang-format off
 #define LAYOUT_all( \
 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013,   \
