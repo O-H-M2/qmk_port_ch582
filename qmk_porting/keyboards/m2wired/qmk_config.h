4dca8b548c531ec6ea7878786ecd45317a696900
OctopusZ <928281509@qq.com>
2023-03-16 22:14:24 +0800

    format qmk_cconfig.h in m2wired

--- qmk_porting/keyboards/m2wired/qmk_config.h
+++ qmk_porting/keyboards/m2wired/qmk_config.h
@@ -121,7 +121,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUXILIARY_RGB_USE_UNIVERSAL_BRIGHTNESS
 #endif
 
-
 #ifdef AW20216
 #define AW_GLOBAL_CURRENT_MAX 255
 #define AW_SCALING_MAX        255
