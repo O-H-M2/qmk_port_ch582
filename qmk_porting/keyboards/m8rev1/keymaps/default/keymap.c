b611471a4ee04d2640642263fcdc4d6722437293
alin elena <alin@elena.space>
2023-04-04 10:39:25 +0100

    sync the main

--- qmk_porting/keyboards/m8rev1/keymaps/default/keymap.c
+++ qmk_porting/keyboards/m8rev1/keymaps/default/keymap.c
@@ -3,6 +3,17 @@
 
 #include QMK_KEYBOARD_H
 
+#ifdef RGBLIGHT_ENABLE
+
+const rgblight_segment_t PROGMEM my_qwerty_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_PURPLE});
+const rgblight_segment_t PROGMEM my_lwr_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_CYAN});
+const rgblight_segment_t PROGMEM my_rse_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_RED});
+const rgblight_segment_t PROGMEM my_adj_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_GREEN});
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(my_qwerty_layer, my_lwr_layer, my_rse_layer, my_adj_layer);
+
+#endif
+
 const uint32_t PROGMEM unicode_map[] = {
     [la]  = 0x03B1 , // α
     [lA]  = 0x0307,  //
@@ -87,6 +98,28 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 }
 
 #endif
+
+#ifdef RGBLIGHT_ENABLE
+
+  void set_rgb_layers(layer_state_t state){
+
+      rgblight_set_layer_state(0, layer_state_cmp(state, _QW));
+      rgblight_set_layer_state(1, layer_state_cmp(state, _LWR));
+      rgblight_set_layer_state(2, layer_state_cmp(state, _RSE));
+      rgblight_set_layer_state(3, layer_state_cmp(state, _ADJ));
+
+  }
+
+  void set_default_rgb_layers(layer_state_t state){
+      rgblight_set_layer_state(0, layer_state_cmp(state, _QW));
+  }
+
+  const rgblight_segment_t * const* my_rgb(void){
+      return my_rgb_layers;
+  }
+
+#endif
+
 
 void keyboard_post_init_user(void) {
 #ifdef RGBLIGHT_ENABLE
--- qmk_porting/keyboards/m8rev1/m8rev1.c
+++ qmk_porting/keyboards/m8rev1/m8rev1.c
@@ -3,17 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-#ifdef RGBLIGHT_ENABLE
-
-const rgblight_segment_t PROGMEM my_qwerty_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_PURPLE});
-const rgblight_segment_t PROGMEM my_lwr_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_CYAN});
-const rgblight_segment_t PROGMEM my_rse_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_RED});
-const rgblight_segment_t PROGMEM my_adj_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, HSV_GREEN});
-
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(my_qwerty_layer, my_lwr_layer, my_rse_layer, my_adj_layer);
-
-#endif
-
 int main()
 {
     extern void protocol_setup();
@@ -35,25 +24,4 @@ int main()
         //! housekeeping_task() is handled by platform
     }
 }
-
-#ifdef RGBLIGHT_ENABLE
-
-  void set_rgb_layers(layer_state_t state){
-
-      rgblight_set_layer_state(0, layer_state_cmp(state, _QW));
-      rgblight_set_layer_state(1, layer_state_cmp(state, _LWR));
-      rgblight_set_layer_state(2, layer_state_cmp(state, _RSE));
-      rgblight_set_layer_state(3, layer_state_cmp(state, _ADJ));
-
-  }
-
-  void set_default_rgb_layers(layer_state_t state){
-      rgblight_set_layer_state(0, layer_state_cmp(state, _QW));
-  }
-
-  const rgblight_segment_t * const* my_rgb(void){
-      return my_rgb_layers;
-  }
-
-#endif
 
