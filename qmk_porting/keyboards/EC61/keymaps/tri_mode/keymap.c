461186d4453e0a1833ff9f7912cc71512c091f78
Wyatt Zhang <46043032+Huckies@users.noreply.github.com>
2023-04-21 10:31:45 +0800

    updated EC61's tri-mode keymap

--- qmk_porting/keyboards/EC61/keymaps/tri_mode/keymap.c
+++ qmk_porting/keyboards/EC61/keymaps/tri_mode/keymap.c
@@ -26,11 +26,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,           KC_N,             KC_M, KC_COMM,  KC_DOT, KC_SLSH, RSFT_T(KC_UP), _______,
                      KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC, KC_RALT, LT(1, KC_LEFT), LT(2, KC_DOWN), RCTL_T(KC_RIGHT)),
     [1] = LAYOUT_all(/* 1: fn */
-                      KC_GRV, BLE_SLOT0, BLE_SLOT1, BLE_SLOT2, BLE_SLOT3, ESB_MODE, _______,  _______, _______, _______, _______,RGB_RMOD, RGB_MOD,  KC_DEL,
-                     _______,   KC_BTN1,   KC_MS_U,   KC_BTN2,   _______,  _______,   KC_P7,    KC_P8,   KC_P9, _______, _______, RGB_VAD, RGB_VAI, _______,
-                       MO(3),   KC_MS_L,   KC_MS_D,   KC_MS_R,   _______,  _______,   KC_P4,    KC_P5,   KC_P6, _______, RGB_HUD, RGB_HUI, RGB_TOG,
-                     _______,   KC_BTN1,   KC_BTN3,   KC_BTN2,   _______,   KC_NUM,   KC_P1,    KC_P2,   KC_P3, RGB_SAD, RGB_SAI, _______, _______,
-                     _______,   _______,   _______,     KC_P0,   _______,  _______, _______,  _______),
+                      KC_GRV, BLE_SLOT0, BLE_SLOT1, BLE_SLOT2, BLE_SLOT3, USB_MODE, ESB_MODE,  _______, _______, _______, _______,RGB_RMOD, RGB_MOD,  KC_DEL,
+                     _______,   KC_BTN1,   KC_MS_U,   KC_BTN2,   _______,  _______,    KC_P7,    KC_P8,   KC_P9, _______, _______, RGB_VAD, RGB_VAI, _______,
+                       MO(3),   KC_MS_L,   KC_MS_D,   KC_MS_R,   _______,  _______,    KC_P4,    KC_P5,   KC_P6, _______, RGB_HUD, RGB_HUI, RGB_TOG,
+                     _______,   KC_BTN1,   KC_BTN3,   KC_BTN2,   _______,   KC_NUM,    KC_P1,    KC_P2,   KC_P3, RGB_SAD, RGB_SAI, _______, _______,
+                     _______,   _______,   _______,     KC_P0,   _______,  _______,  _______,  _______),
     [2] = LAYOUT_all(/* 2: fn */
                       KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,
                      _______, _______,   KC_UP, _______, _______, _______, _______,  KC_INS, KC_HOME, KC_PGUP, _______, _______, _______, _______,
