02580f4549d0d67b0c8601815e5b05ebc4549e91
SharpSnail01 <cxzmemory@qq.com>
2023-05-05 00:28:10 +0800

    Update mcuconf.h

--- qmk_porting/keyboards/imk64/mcuconf.h
+++ qmk_porting/keyboards/imk64/mcuconf.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEBUG_BAUDRATE 460800
 #define DCDC_ENABLE    1
 #define FREQ_SYS       40000000
-#define LSE_ENABLE     0
+#define LSE_ENABLE     1
 #define BLE_SLOT_NUM   4
 ///#define HSE_LOAD_CAPACITANCE 20 // in pF unit
 //#define LSE_LOAD_CAPACITANCE 19 // in pF unit
