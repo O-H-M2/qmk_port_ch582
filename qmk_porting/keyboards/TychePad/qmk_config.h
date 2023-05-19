832f226bf892e5e11ec2fc0c8a1390d11a21094c
OctopusZ <928281509@qq.com>
2023-05-19 16:18:22 +0800

    change BOOTMAGIC_LITE key

--- qmk_porting/keyboards/TychePad/qmk_config.h
+++ qmk_porting/keyboards/TychePad/qmk_config.h
@@ -37,8 +37,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define DYNAMIC_KEYMAP_LAYER_COUNT 10
 
 #define DIODE_DIRECTION       COL2ROW
-#define BOOTMAGIC_LITE_ROW    0
-#define BOOTMAGIC_LITE_COLUMN 0
+#define BOOTMAGIC_LITE_ROW    2
+#define BOOTMAGIC_LITE_COLUMN 1
 // #define PERMISSIVE_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS
 
