fb988e6187e2306b7a63bb907086026bf1909412
Wyatt Zhang <46043032+Huckies@users.noreply.github.com>
2023-05-29 15:17:38 +0800

    made use of pre_process_record_kb()

--- qmk_porting/protocol/extra_keycode.c
+++ qmk_porting/protocol/extra_keycode.c
@@ -23,6 +23,7 @@ extern bool process_ble_passcode_kb(uint16_t keycode, keyrecord_t *record);
 #endif
 
 #if defined BLE_ENABLE || (defined ESB_ENABLE && (ESB_ENABLE == 1 || ESB_ENABLE == 2))
+bool wireless_pre_process_record_kb(uint16_t keycode, keyrecord_t *record);
 bool wireless_process_record(uint16_t keycode, keyrecord_t *record);
 #endif
 
@@ -74,4 +75,15 @@ void post_process_record_kb(uint16_t keycode, keyrecord_t *record)
         process_ble_passcode(keycode, record);
     }
 #endif
+}
+
+bool pre_process_record_kb(uint16_t keycode, keyrecord_t *record)
+{
+    bool ret = wireless_pre_process_record_kb(keycode, record);
+
+    if (!ret) {
+        return false;
+    }
+
+    return pre_process_record_user(keycode, record);
 }
