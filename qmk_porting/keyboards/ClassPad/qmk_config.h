5a3eeeb2fe71b52db0d23e6c654665200b0bc2a9
Wyatt Zhang <46043032+Huckies@users.noreply.github.com>
2023-07-30 20:20:25 +0800

    changed ble indicator config for ClassPad

--- qmk_porting/keyboards/ClassPad/qmk_config.h
+++ qmk_porting/keyboards/ClassPad/qmk_config.h
@@ -72,10 +72,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef RGB_MATRIX_ENABLE
 
 #define BLE_INDICATOR_PREFIX
-#define BLE_SLOT_1_INDICATOR 0
-#define BLE_SLOT_2_INDICATOR 1
-#define BLE_SLOT_3_INDICATOR 2
-#define BLE_SLOT_4_INDICATOR 3
+#define BLE_SLOT_1_INDICATOR 1
+#define BLE_SLOT_2_INDICATOR 2
+#define BLE_SLOT_3_INDICATOR 3
 #define RGB_MATRIX_KEYPRESSES
 // #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_REACTIVE
 #define WS2812_SPI_DRIVER             2
