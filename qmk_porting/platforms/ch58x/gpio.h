28339a5bb0e0e17e194e0e232e054512bcfd6624
Wyatt Zhang <46043032+Huckies@users.noreply.github.com>
2023-10-27 00:56:41 +0800

    improved efficiency of gpio operations

--- qmk_porting/platforms/ch58x/gpio.h
+++ qmk_porting/platforms/ch58x/gpio.h
@@ -31,21 +31,52 @@ typedef struct _gpio_pin_record_t {
 #define GPIO_FIELD_TO_BIT(_field) \
     ((_field) ? 1 : 0)
 
-#define setPinInput(pin)                                                                       \
-    (((pin)&0x80000000) ? (R32_PB_PD_DRV &= ~((pin)&0x7FFFFFFF)) : (R32_PA_PD_DRV &= ~(pin))); \
-    (((pin)&0x80000000) ? (R32_PB_PU &= ~((pin)&0x7FFFFFFF)) : (R32_PA_PU &= ~(pin)));         \
-    (((pin)&0x80000000) ? (R32_PB_DIR &= ~((pin)&0x7FFFFFFF)) : (R32_PA_DIR &= ~(pin)));
-#define setPinInputHigh(pin)                                                                   \
-    (((pin)&0x80000000) ? (R32_PB_PD_DRV &= ~((pin)&0x7FFFFFFF)) : (R32_PA_PD_DRV &= ~(pin))); \
-    (((pin)&0x80000000) ? (R32_PB_PU |= ((pin)&0x7FFFFFFF)) : (R32_PA_PU |= (pin)));           \
-    (((pin)&0x80000000) ? (R32_PB_DIR &= ~((pin)&0x7FFFFFFF)) : (R32_PA_DIR &= ~(pin)));
-#define setPinInputLow(pin)                                                                  \
-    (((pin)&0x80000000) ? (R32_PB_PD_DRV |= ((pin)&0x7FFFFFFF)) : (R32_PA_PD_DRV |= (pin))); \
-    (((pin)&0x80000000) ? (R32_PB_PU &= ~((pin)&0x7FFFFFFF)) : (R32_PA_PU &= ~(pin)));       \
-    (((pin)&0x80000000) ? (R32_PB_DIR &= ~((pin)&0x7FFFFFFF)) : (R32_PA_DIR &= ~(pin)));
-#define setPinOutputPushPull(pin)                                                              \
-    (((pin)&0x80000000) ? (R32_PB_PD_DRV &= ~((pin)&0x7FFFFFFF)) : (R32_PA_PD_DRV &= ~(pin))); \
-    (((pin)&0x80000000) ? (R32_PB_DIR |= ((pin)&0x7FFFFFFF)) : (R32_PA_DIR |= (pin)));
+#define setPinInput(pin)                          \
+    do {                                          \
+        if ((pin)&0x80000000) {                   \
+            R32_PB_PD_DRV &= ~((pin)&0x7FFFFFFF); \
+            R32_PB_PU &= ~((pin)&0x7FFFFFFF);     \
+            R32_PB_DIR &= ~((pin)&0x7FFFFFFF);    \
+        } else {                                  \
+            R32_PA_PD_DRV &= ~(pin);              \
+            R32_PA_PU &= ~(pin);                  \
+            R32_PA_DIR &= ~(pin);                 \
+        }                                         \
+    } while (0);
+#define setPinInputHigh(pin)                      \
+    do {                                          \
+        if ((pin)&0x80000000) {                   \
+            R32_PB_PD_DRV &= ~((pin)&0x7FFFFFFF); \
+            R32_PB_PU |= ((pin)&0x7FFFFFFF);      \
+            R32_PB_DIR &= ~((pin)&0x7FFFFFFF);    \
+        } else {                                  \
+            R32_PA_PD_DRV &= ~(pin);              \
+            R32_PA_PU |= (pin);                   \
+            R32_PA_DIR &= ~(pin);                 \
+        }                                         \
+    } while (0);
+#define setPinInputLow(pin)                      \
+    do {                                         \
+        if ((pin)&0x80000000) {                  \
+            R32_PB_PD_DRV |= ((pin)&0x7FFFFFFF); \
+            R32_PB_PU &= ~((pin)&0x7FFFFFFF);    \
+            R32_PB_DIR &= ~((pin)&0x7FFFFFFF);   \
+        } else {                                 \
+            R32_PA_PD_DRV |= (pin);              \
+            R32_PA_PU &= ~(pin);                 \
+            R32_PA_DIR &= ~(pin);                \
+        }                                        \
+    } while (0);
+#define setPinOutputPushPull(pin)                 \
+    do {                                          \
+        if ((pin)&0x80000000) {                   \
+            R32_PB_PD_DRV &= ~((pin)&0x7FFFFFFF); \
+            R32_PB_DIR |= ((pin)&0x7FFFFFFF);     \
+        } else {                                  \
+            R32_PA_PD_DRV &= ~(pin);              \
+            R32_PA_DIR |= (pin);                  \
+        }                                         \
+    } while (0);
 #define setPinOutputOpenDrain(pin) _Static_assert(0, "WCH platform does not implement an open-drain output")
 #define setPinOutput(pin)          setPinOutputPushPull(pin)
 
@@ -57,29 +88,70 @@ typedef struct _gpio_pin_record_t {
 #define togglePin(pin) (((pin)&0x80000000) ? GPIOB_InverseBits((pin)&0x7FFFFFFF) : GPIOA_InverseBits(pin))
 
 /* self-defined functions */
-#define setPinInterruptRisingEdge(pin)                                                                                                                        \
-    (((pin)&0x80000000) ? (R16_PB_INT_MODE |= ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_MODE |= (pin))); \
-    (((pin)&0x80000000) ? (R32_PB_OUT |= ((pin)&0x7FFFFFFF)) : (R32_PA_OUT |= (pin)));                                                                        \
-    (((pin)&0x80000000) ? (R16_PB_INT_IF = ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_IF = (pin)));       \
-    (((pin)&0x80000000) ? (R16_PB_INT_EN |= ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_EN |= (pin)));
-
-#define setPinInterruptFallingEdge(pin)                                                                                                                       \
-    (((pin)&0x80000000) ? (R16_PB_INT_MODE |= ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_MODE |= (pin))); \
-    (((pin)&0x80000000) ? (R32_PB_CLR |= ((pin)&0x7FFFFFFF)) : (R32_PA_CLR |= (pin)));                                                                        \
-    (((pin)&0x80000000) ? (R16_PB_INT_IF = ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_IF = (pin)));       \
-    (((pin)&0x80000000) ? (R16_PB_INT_EN |= ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_EN |= (pin)));
-
-#define setPinInterruptHighLevel(pin)                                                                                                                             \
-    (((pin)&0x80000000) ? (R16_PB_INT_MODE &= ~(((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14))) : (R16_PA_INT_MODE &= ~(pin))); \
-    (((pin)&0x80000000) ? (R32_PB_OUT |= ((pin)&0x7FFFFFFF)) : (R32_PA_OUT |= (pin)));                                                                            \
-    (((pin)&0x80000000) ? (R16_PB_INT_IF = ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_IF = (pin)));           \
-    (((pin)&0x80000000) ? (R16_PB_INT_EN |= ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_EN |= (pin)));
-
-#define setPinInterruptLowLevel(pin)                                                                                                                              \
-    (((pin)&0x80000000) ? (R16_PB_INT_MODE &= ~(((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14))) : (R16_PA_INT_MODE &= ~(pin))); \
-    (((pin)&0x80000000) ? (R32_PB_CLR |= ((pin)&0x7FFFFFFF)) : (R32_PA_CLR |= (pin)));                                                                            \
-    (((pin)&0x80000000) ? (R16_PB_INT_IF = ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_IF = (pin)));           \
-    (((pin)&0x80000000) ? (R16_PB_INT_EN |= ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14)) : (R16_PA_INT_EN |= (pin)));
+#define setPinInterruptRisingEdge(pin)                                                                       \
+    do {                                                                                                     \
+        if ((pin)&0x80000000) {                                                                              \
+            pin_t pin_int = ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14); \
+                                                                                                             \
+            R16_PB_INT_MODE |= pin_int;                                                                      \
+            R32_PB_OUT |= ((pin)&0x7FFFFFFF);                                                                \
+            R16_PB_INT_IF = pin_int;                                                                         \
+            R16_PB_INT_EN |= pin_int;                                                                        \
+        } else {                                                                                             \
+            R16_PA_INT_MODE |= (pin);                                                                        \
+            R32_PA_OUT |= (pin);                                                                             \
+            R16_PA_INT_IF = (uint16_t)(pin);                                                                 \
+            R16_PA_INT_EN |= (uint16_t)(pin);                                                                \
+        }                                                                                                    \
+    } while (0);
+#define setPinInterruptFallingEdge(pin)                                                                      \
+    do {                                                                                                     \
+        if ((pin)&0x80000000) {                                                                              \
+            pin_t pin_int = ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14); \
+                                                                                                             \
+            R16_PB_INT_MODE |= pin_int;                                                                      \
+            R32_PB_CLR |= ((pin)&0x7FFFFFFF);                                                                \
+            R16_PB_INT_IF = pin_int;                                                                         \
+            R16_PB_INT_EN |= pin_int;                                                                        \
+        } else {                                                                                             \
+            R16_PA_INT_MODE |= (pin);                                                                        \
+            R32_PA_CLR |= (pin);                                                                             \
+            R16_PA_INT_IF = (uint16_t)(pin);                                                                 \
+            R16_PA_INT_EN |= (uint16_t)(pin);                                                                \
+        }                                                                                                    \
+    } while (0);
+#define setPinInterruptHighLevel(pin)                                                                        \
+    do {                                                                                                     \
+        if ((pin)&0x80000000) {                                                                              \
+            pin_t pin_int = ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14); \
+                                                                                                             \
+            R16_PB_INT_MODE &= ~(pin_int);                                                                   \
+            R32_PB_OUT |= ((pin)&0x7FFFFFFF);                                                                \
+            R16_PB_INT_IF = pin_int;                                                                         \
+            R16_PB_INT_EN |= pin_int;                                                                        \
+        } else {                                                                                             \
+            R16_PA_INT_MODE &= ~(pin);                                                                       \
+            R32_PA_OUT |= (pin);                                                                             \
+            R16_PA_INT_IF = (uint16_t)(pin);                                                                 \
+            R16_PA_INT_EN |= (uint16_t)(pin);                                                                \
+        }                                                                                                    \
+    } while (0);
+#define setPinInterruptLowLevel(pin)                                                                         \
+    do {                                                                                                     \
+        if ((pin)&0x80000000) {                                                                              \
+            pin_t pin_int = ((pin)&0x7FFFFFFF) | ((((pin)&0x7FFFFFFF) & (GPIO_Pin_22 | GPIO_Pin_23)) >> 14); \
+                                                                                                             \
+            R16_PB_INT_MODE &= ~(pin_int);                                                                   \
+            R32_PB_CLR |= ((pin)&0x7FFFFFFF);                                                                \
+            R16_PB_INT_IF = pin_int;                                                                         \
+            R16_PB_INT_EN |= pin_int;                                                                        \
+        } else {                                                                                             \
+            R16_PA_INT_MODE &= ~(pin);                                                                       \
+            R32_PA_CLR |= (pin);                                                                             \
+            R16_PA_INT_IF = (uint16_t)(pin);                                                                 \
+            R16_PA_INT_EN |= (uint16_t)(pin);                                                                \
+        }                                                                                                    \
+    } while (0);
 
 bool gpio_record_pin_config(pin_t pin);
 void gpio_recover_pin_config();
