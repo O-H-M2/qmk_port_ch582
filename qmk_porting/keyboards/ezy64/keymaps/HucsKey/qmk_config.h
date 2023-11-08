01d176c1bc166245c3d012e252704f6e8a87ce27
Wyatt Zhang <46043032+Huckies@users.noreply.github.com>
2023-11-08 15:10:20 +0800

    added a new keymap: HucsKey to ezy64

---
+++ qmk_porting/keyboards/ezy64/keymaps/HucsKey/qmk_config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef USB_INDICATOR
+#undef USB_INDICATOR
+#endif
+#ifdef ESB_INDICATOR
+#undef ESB_INDICATOR
+#endif
+
+#define USB_INDICATOR         [1][5]
+#define ESB_INDICATOR         [1][6]
+#define USB_INDICATOR_COLOR_R 0xFF
+#define USB_INDICATOR_COLOR_G 0xFF
+#define USB_INDICATOR_COLOR_B 0xFF
+
+#ifdef RGB_RAW_ENABLE
+#undef AUXILIARY_RGB_USE_UNIVERSAL_BRIGHTNESS
+#endif
